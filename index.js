f68960b9af15cb3ea25635ca097f9e4ee30a920f
root <root@fernando-virtual.(none)>
2018-07-04 11:36:51 -0300

    modified:   index.js 	new file:   modulos/db.js 	modified:   package.json

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var xhub = require('express-x-hub');
+var query = require('./modulos/db');
 
-app.set('port', (process.env.PORT || 8080));
-app.listen(app.get('port'));
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), () => {
+  console.log("Aplicacion DEV-FACEBOOK corriendo en puerto", app.get('port'))
+});
 
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
@@ -28,7 +31,7 @@ var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
-  console.log(req);
+ //console.log(req);
   res.write('<pre> process.env.PORT:' + process.env.PORT + '</pre>');
   res.write('<pre> process.env.PORT:' + process.env.APP_SECRET + '</pre>');
   res.write('<pre> process.env.TOKEN:' + process.env.TOKEN + '</pre>');
@@ -36,39 +39,35 @@ app.get('/', function(req, res) {
   res.end('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
-app.get('/facebooK', function(req, res) {   
+app.get('/facebook', function(req, res) {   
+
+  
   if (
     req.param('hub.mode') == 'subscribe' &&
     req.param('hub.verify_token') == token
   ) {
     res.send(req.param('hub.challenge'));
   } else {
-    res.write('La solicitud GET es incorrecta, verificar campos enviados');
-    res.sendStatus(400);
+    res.sendStatus(400)
   }
+
 });
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
-
+  /*
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
     res.sendStatus(401);
     return;
   }
-
+*/
   console.log('request header X-Hub-Signature validated');
   // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
 
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
+  //received_updates.unshift(req.body);
+    json = query.insertarJSON(req.body);
+    res.sendStatus(200);
 });
 
 app.listen();
